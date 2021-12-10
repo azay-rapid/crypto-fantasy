267ca8c5e34ce688e5a96a18b4229fde1cb2ef0c
a4ay <pradhanajay1999@gmail.com>
2021-12-10 18:53:23 +0530

    added scheduler, prices for start pool and end pool

--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { ABI } from './blockchain.abi';
+import { ABI, AggregatorV3InterfaceABI } from './blockchain.abi';
+import { CronJob } from 'cron';
+import { TokensData } from './tokens-list.data';
 const Web3 = require('web3');
 
 @Injectable()
@@ -18,28 +20,89 @@ export class BlockchainService {
       filter: {
         value: [],
       },
-      fromBlock: 0,
+      fromBlock: 'latest',
     };
 
     //PoolCreated Event
     this.myContract.events
       .poolCreated(options)
-      .on('data', (event) => console.log('data', event))
+      .on('data', (event) => {
+        //start time
+        const startTime = this.cronDateFormatter(
+          new Date(parseInt(event.returnValues.startTime) * 1000),
+        );
+        console.log(startTime);
+        //end time
+        const endTime = this.cronDateFormatter(
+          new Date(parseInt(event.returnValues.endTime) * 1000),
+        );
+        console.log(endTime);
+        const startJob = new CronJob(
+          startTime,
+          startCallback,
+          null,
+          true,
+          'Asia/Kolkata',
+        );
+
+        const endJob = new CronJob(
+          endTime,
+          endCallback,
+          null,
+          true,
+          'Asia/Kolkata',
+        );
+
+        function startCallback() {
+          console.log('start');
+          getCurrentTokenPrices();
+          startJob.stop();
+        }
+
+        function endCallback() {
+          console.log('end');
+          getCurrentTokenPrices();
+          endJob.stop();
+        }
+
+        async function getCurrentTokenPrices() {
+          const web3 = new Web3(
+            'https://data-seed-prebsc-1-s1.binance.org:8545/',
+          );
+          const prices = [];
+          for (let i = 0; i < TokensData.Aggregator.length; i++) {
+            const priceFeed = new web3.eth.Contract(
+              AggregatorV3InterfaceABI,
+              TokensData.Aggregator[i]['address'],
+            );
+
+            await priceFeed.methods
+              .latestRoundData()
+              .call()
+              .then((priceData) => {
+                prices.push(priceData[1]);
+              });
+          }
+
+          console.log(prices);
+        }
+
+        startJob.start();
+        endJob.start();
+      })
       .on('changed', (changed) => console.log('changed', changed))
       .on('error', (err) => {
         throw err;
-      })
-      .on('connected', (str) => console.log(str));
+      });
 
     //Entered Pool
     this.myContract.events
       .enteredPool(options)
-      .on('data', (event) => console.log('data', event))
+      .on('data', (event) => console.log('entered pool', event))
       .on('changed', (changed) => console.log('changed', changed))
       .on('error', (err) => {
         throw err;
-      })
-      .on('connected', (str) => console.log(str));
+      });
 
     //rewardsDistributed
     this.myContract.events
@@ -48,7 +111,27 @@ export class BlockchainService {
       .on('changed', (changed) => console.log(changed))
       .on('error', (err) => {
         throw err;
-      })
-      .on('connected', (str) => console.log(str));
+      });
+  }
+
+  cronDateFormatter(date: Date) {
+    const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hour = date.getHours();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const temp =
+      seconds +
+      ' ' +
+      minutes +
+      ' ' +
+      hour +
+      ' ' +
+      day +
+      ' ' +
+      month +
+      ' ' +
+      '*';
+    return temp;
   }
 }
---
+++ src/blockchain/tokens-list.data.ts
@@ -0,0 +1,169 @@
+export const TokensData = {
+  Aggregator: [
+    {
+      pair: 'AAVE/USD',
+      decimal: '8',
+      address: '0x298619601ebCd58d0b526963Deb2365B485Edc74',
+    },
+    {
+      pair: 'ADA/USD',
+      decimal: '8',
+      address: '0x5e66a1775BbC249b5D51C13d29245522582E671C',
+    },
+    {
+      pair: 'BAKE/USD',
+      decimal: '8',
+      address: '0xbe75E0725922D78769e3abF0bcb560d1E2675d5d',
+    },
+    {
+      pair: 'BCH/USD',
+      decimal: '8',
+      address: '0x887f177CBED2cf555a64e7bF125E1825EB69dB82',
+    },
+    {
+      pair: 'BNB/USD',
+      decimal: '8',
+      address: '0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526',
+    },
+    {
+      pair: 'BTC/ETH',
+      decimal: '18',
+      address: '0x1a602D4928faF0A153A520f58B332f9CAFF320f7',
+    },
+    {
+      pair: 'BUSD/ETH',
+      decimal: '18',
+      address: '0x5ea7D6A33D3655F661C298ac8086708148883c34',
+    },
+    {
+      pair: 'BUSD/USD',
+      decimal: '8',
+      address: '0x9331b55D9830EF609A2aBCfAc0FBCE050A52fdEa',
+    },
+    {
+      pair: 'CAKE/USD',
+      decimal: '8',
+      address: '0x81faeDDfeBc2F8Ac524327d70Cf913001732224C',
+    },
+    {
+      pair: 'CREAM/USD',
+      decimal: '8',
+      address: '0xB8eADfD8B78aDA4F85680eD96e0f50e1B5762b0a',
+    },
+    {
+      pair: 'DAI/BNB',
+      decimal: '18',
+      address: '0x0630521aC362bc7A19a4eE44b57cE72Ea34AD01c',
+    },
+    {
+      pair: 'DAI/USD',
+      decimal: '8',
+      address: '0xE4eE17114774713d2De0eC0f035d4F7665fc025D',
+    },
+    {
+      pair: 'DODO/USD',
+      decimal: '8',
+      address: '0x2939E0089e61C5c9493C2013139885444c73a398',
+    },
+    {
+      pair: 'DOGE/USD',
+      decimal: '8',
+      address: '0x963D5e7f285Cc84ed566C486c3c1bC911291be38',
+    },
+    {
+      pair: 'DOT/USD',
+      decimal: '8',
+      address: '0xEA8731FD0685DB8AeAde9EcAE90C4fdf1d8164ed',
+    },
+    {
+      pair: 'EQZ/USD',
+      decimal: '8',
+      address: '0x6C2441920404835155f33d88faf0545B895871b1',
+    },
+    {
+      pair: 'ETH/USD',
+      decimal: '8',
+      address: '0x143db3CEEfbdfe5631aDD3E50f7614B6ba708BA7',
+    },
+    {
+      pair: 'FIL/USD',
+      decimal: '8',
+      address: '0x17308A18d4a50377A4E1C37baaD424360025C74D',
+    },
+    {
+      pair: 'FRONT/USD',
+      decimal: '8',
+      address: '0x101E51C0Bc2D2213a9b0c991A991958aAd3fF96A',
+    },
+    {
+      pair: 'INJ/USD',
+      decimal: '8',
+      address: '0x58b299Fa027E1d9514dBbEeBA7944FD744553d61',
+    },
+    {
+      pair: 'LINK/BNB',
+      decimal: '18',
+      address: '0x351Ff08FF5077d6E8704A4763836Fe187f074380',
+    },
+    {
+      pair: 'LINK/USD',
+      decimal: '8',
+      address: '0x1B329402Cb1825C6F30A0d92aB9E2862BE47333f',
+    },
+    {
+      pair: 'LTC/USD',
+      decimal: '8',
+      address: '0x9Dcf949BCA2F4A8a62350E0065d18902eE87Dca3',
+    },
+    {
+      pair: 'MATIC/USD',
+      decimal: '8',
+      address: '0x957Eb0316f02ba4a9De3D308742eefd44a3c1719',
+    },
+    {
+      pair: 'REEF/USD',
+      decimal: '8',
+      address: '0x902fA2495a8c5E89F7496F91678b8CBb53226D06',
+    },
+    {
+      pair: 'SFP/USD',
+      decimal: '8',
+      address: '0x4b531A318B0e44B549F3b2f824721b3D0d51930A',
+    },
+    {
+      pair: 'SXP/USD',
+      decimal: '8',
+      address: '0x678AC35ACbcE272651874E782DB5343F9B8a7D66',
+    },
+    {
+      pair: 'TRX/USD',
+      decimal: '8',
+      address: '0x135deD16bFFEB51E01afab45362D3C4be31AA2B0',
+    },
+    {
+      pair: 'TWT/USD',
+      decimal: '8',
+      address: '0x7671d7EDb66E4C10d5FFaA6a0d8842B5d880F0B3',
+    },
+    {
+      pair: 'USDC/USD',
+      decimal: '8',
+      address: '0x90c069C4538adAc136E051052E14c1cD799C41B7',
+    },
+    {
+      pair: 'USDT/USD',
+      decimal: '8',
+      address: '0xEca2605f0BCF2BA5966372C99837b1F182d3D620',
+    },
+    {
+      pair: 'XRP/USD',
+      decimal: '8',
+      address: '0x4046332373C24Aed1dC8bAd489A04E187833B28d',
+    },
+    {
+      pair: 'XVS/USD',
+      decimal: '8',
+      address: '0xCfA786C17d6739CBC702693F23cA4417B5945491',
+    },
+  ],
+};
