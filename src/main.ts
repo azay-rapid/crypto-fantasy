31f51862232fd9543778f01419f60bb74f9df985
a4ay <pradhanajay1999@gmail.com>
2021-12-28 16:42:23 +0530

    fix:update cors policy

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const whitelist = [
+    'http://localhost:3000',
+    'https://master.d3sg8fr1hdgwhn.amplifyapp.com/',
+  ];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
