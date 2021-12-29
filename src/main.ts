8a902cddf53e1151a9f86b2c2952d2a0530f418c
a4ay <pradhanajay1999@gmail.com>
2021-12-29 10:22:34 +0530

    fix: cors whitelist link

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const whitelist = [
     'http://localhost:3000',
-    'https://master.d3sg8fr1hdgwhn.amplifyapp.com/',
+    'https://master.d3sg8fr1hdgwhn.amplifyapp.com',
   ];
   app.enableCors({
     origin: function (origin, callback) {
